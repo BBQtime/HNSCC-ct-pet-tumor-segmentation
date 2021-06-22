c9c8c0998b2b55f07dd86b77e0be5bad9d621d77
jintaoren <jintaodk@gmail.com>
2021-06-22 10:55:47 +0200

    add PET cliper

---
+++ pet_cliper.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import SimpleITK as sitk
+import os
+from multiprocessing import Pool
+import glob
+import numpy as np
+import argparse
+import path
+
+
+def pet_cliper(patient):
+
+    print("processing patient no. ", patient)
+
+    PET_path = path_in + patient +'_pt.nii.gz'
+
+    pet_img = sitk.ReadImage(PET_path)
+    pet = sitk.GetArrayFromImage(pet_img)
+
+    if mode == 'quantile':
+        ptc = np.clip(pet, pet.min(), np.quantile(pet, clip))
+    elif clip_mode == 'number':
+        ptc = np.clip(pet, pet.min(), clip)
+
+    out_file = path_in + patient +'_ptc.nii.gz'
+
+    img_corr = sitk.GetImageFromArray(ptc)
+    img_corr.CopyInformation(pet_img)
+    sitk.WriteImage(img_corr, out_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--mode", type=str, default='quantile',
+                            help="clip mode choose from \'quantile\' and \'number\'")
+    parser.add_argument("--clip", type=float, default=0.98,
+                            help="clip value")
+    args = parser.parse_args()
+    # path_in = "/mnt/faststorage/jintao/HNSCC/hecktor2021_train/resampled/" #change to your train folder
+    path_in  = path.resampled_path
+    clip = args.clip
+    mode = args.mode
+
+
+    print('working direcotroy is: ', path_in)
+    file_list = glob.glob(os.path.join(path_in, '*'))
+
+    patient_names = sorted( list(set(os.path.basename(patient)[:7] for patient in file_list)))
+    print("patient names", patient_names)
+
+    # parameterlist = []
+    # for index, _ in enumerate(patient_names):
+    #     parameterlist.extend([[patient_names[index]]])
+    # print(len(parameterlist))
+
+
+    p = Pool(processes=8)              # start 8 worker processes
+    #print(parameterlist)
+    p.map(pet_cliper, patient_names)
+    p.close()
--- pet_face_mask_filter.py
+++ pet_face_mask_filter.py
@@ -4,24 +4,18 @@ import os
 from multiprocessing import Pool
 import glob
 import numpy as np
+import path
+
+#path_in = "/mnt/faststorage/jintao/HNSCC/hecktor2021_train/resampled/" #change to your train folder
 
 
-path_in = "/mnt/faststorage/jintao/HNSCC/hecktor2021_train/resampled/" #change to your train folder
-
-
-
-file_list = glob.glob(os.path.join(path_in, '*'))
-
-patient_names = sorted( list(set(os.path.basename(pt)[:7] for pt in file_list)))
-print("patient names", patient_names)
-
-def face_mask_filter(pt):
+def face_mask_filter(patient):
     #_ct.nii.gz
 
-    print("processing patient no. ", pt)
+    print("processing patient no. ", patient)
 
-    CT_path = path_in + pt +'_ct.nii.gz'
-    PET_path = path_in + pt +'_pt.nii.gz'
+    CT_path = path_in + patient +'_ct.nii.gz'
+    PET_path = path_in + patient +'_pt.nii.gz'
 
 
     pet_img = sitk.ReadImage(PET_path)
@@ -34,18 +28,26 @@ def face_mask_filter(pt):
 
     PTfm = pet*fm
 
-    out_file = path_in + pt +'_ptfx.nii.gz'
+    out_file = path_in + patient +'_ptfx.nii.gz'
 
     img_corr = sitk.GetImageFromArray(PTfm)
     img_corr.CopyInformation(pet_img)
     sitk.WriteImage(img_corr, out_file)
 
+if __name__ == "__main__":
+    path_in  = path.resampled_path
 
-# parameterlist = []
-# for index, _ in enumerate(patient_names):
-#     parameterlist.extend([[patient_names[index]]])
-# print(len(parameterlist))
+    file_list = glob.glob(os.path.join(path_in, '*'))
 
-p = Pool(processes=8)              # start 8 worker processes
-#print(parameterlist)
-p.map(face_mask_filter, patient_names)
+    patient_names = sorted( list(set(os.path.basename(patient)[:7] for patient in file_list)))
+    print("patient names", patient_names)
+
+    # parameterlist = []
+    # for index, _ in enumerate(patient_names):
+    #     parameterlist.extend([[patient_names[index]]])
+    # print(len(parameterlist))
+
+    p = Pool(processes=8)              # start 8 worker processes
+    #print(parameterlist)
+    p.map(face_mask_filter, patient_names)
+    p.close()
