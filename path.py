d0c2d129b6f3fd18cd0795a41b87c689e48cade9
Jintao Ren <jintaodk@gmail.com>
2021-07-15 16:45:40 +0200

    move to preprossing

--- path.py
+++
@@ -1,4 +0,0 @@
-import numpy as np 
-import os
-
-resampled_path =  "/mnt/faststorage/jintao/HNSCC/hecktor2021_train/resampled/" #change to your train folder
---
+++ preprocessing/path.py
@@ -0,0 +1,4 @@
+import numpy as np 
+import os
+
+resampled_path =  "/mnt/faststorage/jintao/HNSCC/hecktor2021_train/resampled/" #change to your train folder
